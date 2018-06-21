df8f874b177977ae293c92a4911fd7c083f82f5d
EFinish <EFinish@gmail.com>
2018-06-21 14:26:50 -0500

    form one with 3 first inputs and their data bindings

--- vue/js/script.js
+++ vue/js/script.js
@@ -8,6 +8,46 @@ Vue.component(
     }
 )
 
+Vue.component(
+    'form-one',
+    {
+        data: function() {
+            return {
+                name: '',
+                age: '',
+                birthday: '',
+                gender: '',
+                niceDay: ''
+            }
+        },
+        template:
+        '<div>'+
+            '<div>'+
+                '<label for="">What is your name?</label>'+
+                '<input id="" type="text" placeholder="Enter your name..." v-model="name">'+
+                '{{name}}'+
+            '</div>'+
+            '<div>'+
+                '<label for="">What is your age?</label>'+
+                '<input id="" type="number" min="0" placeholder="Enter your age..." v-model="age">'+
+                '{{age}}'+
+            '</div>'+
+            '<div>'+
+                '<label for="">What is your birthday?</label>'+
+                '<input id="" type="date" placeholder="mm/dd/yyyy" aria-label="What is your birthday (mm/dd/yyyy)?" v-model="birthday">'+
+                '{{birthday}}'+
+            '</div>'+
+        '</div>'
+    }
+)
+
 var pp = new Vue({
-    el: "#app"
+    el: "#app",
+    data: {
+        name: '',
+        age: '',
+        birthday: '',
+        gender: '',
+        niceDay: ''
+    }
 })
