aa001b27b05a6a32af276e46072d8d477d7bddec
EFinish <EFinish@gmail.com>
2018-06-21 13:55:50 -0500

    vue index and js, first vue component is the "Vue Example" message

---
+++ vue/js/script.js
@@ -0,0 +1,13 @@
+Vue.component(
+    "vue-example-header",
+    {
+        template: 
+        "<div>"+
+        "<h1>Vue.js Example</h1>"+
+        "</div>"
+    }
+)
+
+var pp = new Vue({
+    el: "#app"
+})
